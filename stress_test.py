822bacf20af844a2eecfa0985dc9f583a247c8c6
Nick Young <nick.young@auckland.ac.nz>
2020-11-30 16:37:55 +1300

    simplify stress_test.py

--- stress_test.py
+++ stress_test.py
@@ -5,50 +5,47 @@ import websockets
 import json
 import random
 import time
-import pandas as pd
 
 URI = "wss://api-proxy.auckland-cer.cloud.edu.au/dynamic_network_graph"
 #URI = "ws://api-proxy.auckland-cer.cloud.edu.au:6789"
-#URI = "ws://localhost:6789"
+URI = "ws://localhost:6789"
 SESSION_ID = "STRESS_TEST"
-MAX_CLIENTS = 1000
+connections = []
 
-async def test():
+async def read_all(websocket):
+    try:
+        while True:
+            await asyncio.wait_for(websocket.recv(), 0)
+    except:
+        return
+
+async def test(i):
     start = time.time()
-    async with websockets.connect(URI) as websocket:
-        ttc = time.time() - start
-        await websocket.send(json.dumps({
-            "action": "connect",
-            "session_id": SESSION_ID
-        }))
-        await websocket.send(json.dumps({
-            "session_id": SESSION_ID,
-            "action": "upsert_entry",
-            "entry": {
-                "id": random.randint(0, 100),
-                "donor": random.randint(0, 100),
-                "resourceType": "$",
-                "recipient": random.randint(0, 100)
-            }
-        }))
-        tts = time.time() - start
-        try:
-            while True:
-                await asyncio.wait_for(websocket.recv(), 0)
-        except asyncio.exceptions.TimeoutError:
-            pass
-        ttr = time.time() - start
-        await websocket.close()
-        tte = time.time() - start
-        return {"ttc": ttc, "tts": tts, "ttr": ttr, "tte": tte}
+    websocket = await websockets.connect(URI)
+    connections.append(websocket)
+    await websocket.send(json.dumps({
+        "action": "connect",
+        "session_id": SESSION_ID
+    }))
+    await websocket.send(json.dumps({
+        "session_id": SESSION_ID,
+        "action": "upsert_entry",
+        "entry": {
+            "id": random.randint(0, 100),
+            "donor": random.randint(0, 100),
+            "resourceType": "$",
+            "recipient": random.randint(0, 100)
+        }
+    }))
+    return time.time() - start
 
-async def run_n_tests(n):
-    results = await asyncio.gather(*[test() for i in range(n)])
-    df = pd.DataFrame(results)
-    return df
-
-print("n_clients,mttc,mtts,mttr,mtte,wall_time")
-for n_clients in range(10, MAX_CLIENTS, 100):
+async def main():
+    print("n_clients,t,wall_time")
     start = time.time()
-    df = asyncio.run(run_n_tests(n_clients))
-    print(f"{len(df)},{','.join(str(x) for x in df.mean().values)},{time.time() - start}")
+    for i in range(1000):
+        result = await test(i)
+        print(f"{len(connections)},{result},{time.time() - start}")
+        for ws in connections:
+            await read_all(ws)
+
+asyncio.run(main())
